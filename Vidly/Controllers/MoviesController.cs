dd20d58b97ae40ce67566db71ba26fd1c10e11c9
ZSSVE <zhaoshuosve@gmail.com>
2017-06-01 17:23:44 -0500

    add customer and movies

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,59 +11,35 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            var movies = new List<Movie>
-            {
-                new Movie {Name = "Minion", Id = 1},
-                new Movie {Name = "SHrek", Id = 2}
-            };
+        private ApplicationDbContext _context;
 
-            return View(new MovieList {Movies = movies});
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        public ActionResult Index()
+        {
+            var movies = _context.Movies.Include(c => c.Genre).ToList();
+
+            return View(movies);
 
         }
 
-// GET: Movies
-        public ActionResult Random()
+
+        public ActionResult Details(int id)
         {
-            var movie = new Movie() {Name = "Minions"};
-            var customers = new List<Customer>
-            {
-                new Customer {Name = "Alicia"},
-                new Customer {Name = "Lily"},
-                new Customer {Name = "Peter"}
-            };
+            var movie = _context.Movies.Include(c => c.Genre).SingleOrDefault(c => c.Id == id);
+            if (movie == null)
+                return HttpNotFound();
+            return View(movie);
 
-            var randomMovieView = new RandomMovieViewModel
-            {
-                Movie = movie,
-                Customers = customers
-            };
-
-            return View(randomMovieView);
         }
-
-//        public ActionResult Edit(int movieId)
-//        {
-//            return Content("movieId: " + movieId);
-//        }
-//
-//        public ActionResult Index(int? pageIndex, string sortBy)
-//        {
-//            if (!pageIndex.HasValue)
-//            {
-//                pageIndex = 1;
-//            }
-//
-//            if (String.IsNullOrWhiteSpace(sortBy))
-//            {
-//                sortBy = "Name";
-//            }
-//
-//            return Content(String.Format("pageIndex = {0} & sortBy = {1}", pageIndex, sortBy));
-//
-//        }
-
 
         [Route("movies/released/{year:min(2000):max(2017)}/{month:regex(\\d{2}):range(1,12)}")]
         public ActionResult ByReleaseDate(int year, int month)
